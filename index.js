fa6872f39cc9e1fd8b20c8f0c4402662d607aa93
hmbashar <hmbashar@gmail.com>
2025-06-20 16:28:18 +0600

    server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+

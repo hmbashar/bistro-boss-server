b60bffe8454a069aa1c4f1f9a5814a447c67b57a
hmbashar <hmbashar@gmail.com>
2025-07-06 14:18:27 +0600

    post and get for carts

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+//middleware
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 3000;
+
 
 
 
@@ -24,11 +28,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const menuCollection = client.db("Bistro").collection("Menu");
+    const ReviewsCollection = client.db("Bistro").collection("Reviews");
+    const CartCollection = client.db("Bistro").collection("Carts");
 
+    
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result);
     })
+
+    app.get('/reviews', async(req, res) => {
+        const result = await ReviewsCollection.find().toArray();
+        res.send(result);
+    })
+
+      //cart collection
+    app.post('/carts', async(req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await CartCollection.insertOne(item);
+      res.send(result)
+    })
+
+    //get cart collection
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await CartCollection.find(query).toArray();      
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -40,9 +69,6 @@ async function run() {
 run().catch(console.dir);
 
 
-//middleware
-app.use(cors());
-app.use(express.json());
 
 
 

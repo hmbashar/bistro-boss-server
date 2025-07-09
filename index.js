f24aa2f9eccacef9d53334ee6a5161bb882e1051
hmbashar <hmbashar@gmail.com>
2025-07-10 01:53:05 +0600

    craeted API for display users, delete user, make admin

--- index.js
+++ index.js
@@ -33,6 +33,35 @@ async function run() {
     const ReviewsCollection = client.db("Bistro").collection("Reviews");
     const CartCollection = client.db("Bistro").collection("Carts");
 
+
+    //all users
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    //user delete
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    //make admin
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    })
+
+
     //post user
     app.post('/users', async(req, res) => {
       const user = req.body;
